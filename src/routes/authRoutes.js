3703c44dc57293b9b19660cd59e025bb4185cad0
Leonard <leonardwkee@gmail.com>
2021-12-29 20:22:02 -0800

    part 14.12 - 14.22: add calls for track

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 const User = mongoose.model('User')
 const router = express.Router()
@@ -10,8 +11,33 @@ router.post('/signup', async (req, res) => {
   try {
     const user = new User({ email, password })
     await user.save()
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET)
+    res.send({ token })
   } catch (err) {
     return res.status(422).send(err.message)
+  }
+})
+
+router.post('/signin', (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password. ' })
+  }
+
+  await User.findOne({ email })
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid email or password.' })
+  }
+
+  try {
+    await user.comparePassword(password)
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET)
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid email or password.' })
   }
 })
 
